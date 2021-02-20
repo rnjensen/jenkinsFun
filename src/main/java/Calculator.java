5402739e5e59416076b6e56a7277b2f61c93e075
rnjensen <rneweljensen@gmail.com>
2021-02-19 20:49:39 -0700

    random string stuff

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.UUID;
 
 class Calculator {
 
@@ -74,8 +75,7 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-
-        return null;
+        return n + UUID.randomUUID().toString();
     }
 
 
